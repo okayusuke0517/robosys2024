b229c0fd35e50a278a4d5f4ed5bf1cf6202cb10c
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2024-11-24 23:52:16 +0900

    k1.py push_2

--- k1.py
+++ k1.py
@@ -3,25 +3,20 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import random
-
+import time
 def play_game():
     directions = ['上','下','左','右']
+    
 
-    for i in range(1, 4):
-        input(f"\n{i}回目: Enterを押すと開始")
-        print(f"あっち向いてホイ　> {random.choice(directions)}")
+    print("自動的に開始")
+    for i in range(3):
+        print("あっち向いてホイ")
+        time.sleep(3)
+        print(f"　-> {random.choice(directions)}")
 
-        
-        while True:
-            user_input = input("勝っていたらｙ、負けていたらｎをおせ")
-            if user_input == 'y':
-                print("あなたの勝ち")
-                break
-            elif user_input == 'n':
-                print("コンピュータの勝ち")
-                break
-
-    input("\n3回終わったのでEnterを押して終了")
+                
+    time.sleep(2)
+    print("\n終了")
 
 if __name__ =="__main__":
     play_game()
