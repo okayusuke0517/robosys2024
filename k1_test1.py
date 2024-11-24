b8bfdf7131fa1e9c744a0bb768eb384f76b000c8
okayusuke2024 <s23C1031KK@s.chibakoudai.jp>
2024-11-24 23:14:17 +0900

    k1_test1.py push

---
+++ k1_test1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024 Yusuke Oka
+# SPDX-License-Identifier: BSD-3-Clause
+
+import random  
+import time
+
+
+def k1_test1():
+    directions = ['上','下','左','右']
+
+    print("自動的に開始")
+
+if __name__ =="__main__":
+    k1_test1()
+
+
